63af8984e82ec05d3f9de08d51c04eb9e95e67d9
janrzepkowski <jan.rzepkowski21@gmail.com>
2025-06-08 13:31:11 +0200

    60 coins per page

--- src/App.jsx
+++ src/App.jsx
@@ -64,7 +64,7 @@ function App() {
     try {
       setLoading(true);
       const response = await axios.get(
-        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=50&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C14d%2C30d%2C200d%2C1y",
+        "https://api.coingecko.com/api/v3/coins/markets?vs_currency=usd&order=market_cap_desc&per_page=60&page=1&sparkline=false&price_change_percentage=1h%2C24h%2C7d%2C14d%2C30d%2C200d%2C1y",
         options
       );
       setCoins(response.data);
